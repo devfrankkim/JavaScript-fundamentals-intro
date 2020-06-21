fbe07534f9e5383b1243365f64f2ae37172b6098
heyfranksmile@gmail.com <heyfranksmile@gmail.com>
2020-06-21 15:34:33 -0400

    Null and Undefined

---
+++ Null-undefined.js
@@ -0,0 +1,21 @@
+// Null and Undefined
+
+// both represent "no value"
+
+// undefined => "Hey!! JavaScipt can not find value for this thing"
+
+// null => "Developer sets the value"
+
+/*
+     "undefined"
+
+    - variable without value
+    - missing function arguments
+    - missing object properties
+*/
+
+let numberNull = 20 + null; // 20 + 0; => NULL is something that we set
+console.log(numberNull); // 20
+
+let numberUndfined = 20 + undefined; //
+console.log(numberUndfined); // NaN  => set by JavaScript, it can't find values

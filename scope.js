dbddb53c35c324ec4d9d112ed2a8001ce9f71586
heyfranksmile@gmail.com <heyfranksmile@gmail.com>
2020-06-21 15:43:34 -0400

    Global Scope vs Local Scope

--- scope.js
+++ scope.js
@@ -8,18 +8,20 @@
 let globalName = "frank";
 globalName = "andrea";
 
+console.log(globalName); // andrea (1)
+
 // Hey! What's your issue? you already created the variable!!
 // const name = "tessa";
 
 // calculate function
 function calculate() {
-  console.log(globalName); // Before console.log
+  console.log(globalName); // Before console.log (4)
   globalName = "After";
-  console.log(globalName); // After
+  console.log(globalName); // After (5)
 
   function inner() {
     globalName = "inside inner function";
-    console.log(globalName); // inside inner function
+    console.log(globalName); // inside inner function (6)
   }
   inner();
 }
@@ -28,8 +30,9 @@ function calculate() {
 
 if (true) {
   globalName = "Before console.log";
-  console.log(globalName); // Before console.log
+  console.log(globalName); // Before console.log (2)
 }
+console.log(globalName); // Before console.log (3)
 
 calculate();
 
@@ -37,25 +40,27 @@ calculate();
 
 let localName = "I am the local dude";
 
+console.log(localName); // "I am the local dude"
+
 function calculateLocal() {
   // The variable only works here
 
   const localName = "frank";
   const age = 100;
-  console.log(localName, age); // "frank"
+  console.log(localName, age); // "frank, 100"
 
   // Global Scope, to protect the variable, use let or const
   becomesGlobal = "global variable";
 }
 
 console.log(localName); // I am the local dude
-console.log(becomesGlobal); // becomesGlobal is not defined
+console.log(becomesGlobal); // becomesGlobal is not defined because the function isn't invoked.
 console.log(age); // age is not defined
 
 calculateLocal();
 console.log(age); // age is not defined
 
-// after invoking the function, you can grab the variable inside the function
+// after invoking the function, you can grab the global variable inside the function
 console.log(becomesGlobal); // global variable
 
 if (true) {
@@ -66,6 +71,6 @@ if (true) {
 {
   const localName = "andrea";
   const special = "special";
-  console.log(localName, special);
+  console.log(localName, special); // "andrea special"
 }
-console.log(localName, special); // special is not defined
+console.log(localName, special); // localName & special is not defined

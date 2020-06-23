e773899d50969bc72d849fbf9d4c6fe82b2215f0
heyfranksmile@gmail.com <heyfranksmile@gmail.com>
2020-06-23 19:24:28 -0400

    Accept CALLBACK function as an Argument

---
+++ array-iterators.js
@@ -0,0 +1,11 @@
+// Accept CALLBACK function as an Argument
+// and then calls CALLBACK against each item in an array.
+// Reference Item in the CALLBACK Parameter.
+
+const numbers = [0, 1, 2, 3, 4];
+
+// show all numbers
+
+for (let i = 0; i < numbers.length; i++) {
+  console.log(numbers[i]);
+}

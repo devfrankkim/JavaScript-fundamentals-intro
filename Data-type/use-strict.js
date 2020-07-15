aeb76568452e7e5e3f0cd8ecf0f6e3855861c875
heyfranksmile@gmail.com <heyfranksmile@gmail.com>
2020-07-15 12:47:55 -0400

    use strict

---
+++ Data-type/use-strict.js
@@ -0,0 +1,20 @@
+// ========= USE strict =========
+// added in ES5
+// use this for vanilla JavaScript.
+// Also note you can apply "strict mode" to the whole file... Or you can use it only for a specific function
+"use strict";
+
+// Non-strict code...
+
+(function () {
+  "use strict";
+
+  // Define your library strictly...
+
+})();
+
+// Non-strict code...
+
+// Whole-script strict mode syntax
+("use strict");
+var v = "Hi! I'm a strict mode script!";

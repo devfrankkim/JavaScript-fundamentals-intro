ee814874521c0639d579344a72a34f1512e57ab1
heyfranksmile@gmail.com <heyfranksmile@gmail.com>
2020-07-15 12:47:17 -0400

    string

---
+++ Data-type/string.js
@@ -0,0 +1,9 @@
+// String
+
+const char = "c";
+const frank = "frank";
+const greeting = "hello" + frank;
+console.log(`value: ${greeting}, type: ${typeof greeting}`); // value: hellofrank, type: string
+
+const helloFrank = `hi ${frank}`;
+console.log(`value: ${helloFrank}, type: ${typeof helloFrank}`); // value: hi frank, type: string

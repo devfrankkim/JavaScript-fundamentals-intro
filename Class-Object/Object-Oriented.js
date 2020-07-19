465ca0ac35c9e9393564ffb307e23fdd2298e42c
heyfranksmile@gmail.com <heyfranksmile@gmail.com>
2020-07-19 11:15:45 -0400

    Class VS Object

---
+++ Class-Object/Object-Oriented.js
@@ -0,0 +1,33 @@
+class Person{
+    name; // field
+    age; // field
+    speak(); // method
+}
+
+// class {field, methods} 
+// fields only -> data class
+
+/*
+관련 있는 변수 함수 -> 클래스
+클래스 내부적으로 보여지는 변수, 외부적으로 보여지는 변수 나눈다 -> (캡슐화)
+상속, 다향성
+객체 지향 언어
+
+=========== Class ===========
+
+붕어빵을 만들 수 있는 틀
+템플릿만 정의해 논다. 어떤 데이터가 들어 올 수 있는지. 진짜 데이터는 있지 않다.
+Class (blue print) 실제 메모리에 올라가지 않는다. 정의만 한다.
+    - template 
+    - declare once
+    - NO DATA INSIDE 
+
+===========   Object ===========
+
+(팥 붕어빵)
+클래스를 이용해서, 새로운 인스턴스를 생성하면, Object가 된다.
+Object 실제 데이터를 넣으면, 메모리에 올라간다. 
+    - instance of a class
+    - created many times
+    - DATA INSIDE
+*/

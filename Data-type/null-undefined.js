79484ae39798543b7adf587d2474835de29916b8
heyfranksmile@gmail.com <heyfranksmile@gmail.com>
2020-07-25 12:18:45 -0400

    Difference between null vs undefined

---
+++ Data-type/null-undefined.js
@@ -0,0 +1,9 @@
+// null
+// YOU are pointing the empty value
+let nothing = null;
+console.log(`value: ${nothing}, type: ${typeof nothing}`); // value: null, type: object
+
+// undefined
+// declared but computer is pointing the no value
+let noneDefined;
+console.log(`value: ${noneDefined}, type: ${typeof noneDefined}`); // value: undefined, type: undefined

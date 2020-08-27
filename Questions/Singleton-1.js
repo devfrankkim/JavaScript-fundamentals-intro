935adce007b862fb3e1300adc84f24de3cc30052
heyfranksmile@gmail.com <heyfranksmile@gmail.com>
2020-08-27 16:04:17 -0400

    Singleton Return

--- Questions/Singleton-1.js
+++ Questions/Singleton-1.js
@@ -8,10 +8,42 @@ let obj = (function () {
   let objInstance; //private variable
   function create() {
     //private function to create methods and properties
+    let _isRunning = false;
+    let start = () => {
+      {
+        _isRunning = true;
+      }
+    };
+    let stop = () => {
+      {
+        _isRunning = false;
+      }
+    };
+    let currentState = () => {
+      {
+        return _isRunning;
+      }
+    };
+    return { start: start, stop: stop, currentState: currentState };
   }
 
-  return {};
+  return {
+    getInstance: () => {
+      if (!objInstance) {
+        objInstance = create();
+      }
+      console.log(objInstance); // {start: ƒ, stop: ƒ, currentState: ƒ}
+      return objInstance;
+    },
+  };
 })();
-console.log(obj);
+
 let obj1 = obj.getInstance();
-// let obj2 = obj.getInstance();
+let obj2 = obj.getInstance();
+
+obj1 === obj2; // true
+
+obj1.currentState(); // false
+obj2.start();
+obj1.currentState(); // true
+obj2.currentState(); // true

044a815ed9b112e61dc92d94d6e73565e95cc3ef
heyfranksmile@gmail.com <heyfranksmile@gmail.com>
2020-09-21 17:31:39 -0400

    this_1

---
+++ this/this_1.js
@@ -0,0 +1,46 @@
+// ========== problems =========
+var contact = {
+  firstName: "frank",
+  printName: function () {
+    // console.log(firstName);  // firstName is not defined
+    console.log(contact.firstName);
+  },
+};
+
+contact.printName(); // frank
+
+var newContact = contact;
+contact = null;
+newContact.printName(); // Cannot read property 'firstName' of null
+
+// ============= What if there's super keyword to point to any variables? ===========
+
+var contact = {
+  firstName: "frank",
+  printName: function () {
+    console.log(this.firstName);
+  },
+};
+contact.printName(); // frank
+
+var one = contact;
+var two = contact;
+var anyKeyword = contact;
+
+contact = null;
+
+one.printName(); // frank
+two.printName(); // frank
+anyKeyword.printName(); // frank
+
+var contact = {
+  firstName: "frank",
+  printName: function () {
+    console.log(this.firstName);
+  },
+};
+function add(x, y, callback) {
+  console.log(x + y);
+  callback();
+}
+add(1, 2, contact.printName); // 3, undefined

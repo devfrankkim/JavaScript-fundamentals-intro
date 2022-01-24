23b8c770a6f88145d2501f92bf824af7f47ccba8
heyfranksmile@gmail.com <heyfranksmile@gmail.com>
2022-01-23 22:00:00 -0500

    boolean

---
+++ Data-type/Boolean.js
@@ -0,0 +1,8 @@
+// boolean
+// false => 0, null, undefined, NaN, ''
+// true => any other values
+
+const canRead = true; // true
+const test = 3 < 1; // false
+console.log(`value: ${canRead}, type: ${typeof canRead}`);
+console.log(`value: ${test}, type: ${typeof test}`);
